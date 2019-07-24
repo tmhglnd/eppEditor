e6fcccc0bf73a12c89073186319a66665381c0a1
tmhglnd <timohoogland@gmail.com>
2019-07-24 18:05:08 +0100

    added oscbank function to IR.js

--- src/IR/IR.js
+++ src/IR/IR.js
@@ -18,20 +18,18 @@ const jsFuncMap = {
         ${o}.phaseReset(${p.length>2 ? p[1].loop : 0.0});
         ${o}mod.phaseReset(${p.length>2 ? p[1].loop : 0.0});`,
     "loop":(o,p) => `${o}.sinewave(${p[0].loop})*${o}mod.sinewave(${p[1].loop})`},
-    'fmsynth': {
-      "setup":(o,p) => `${o} = new Module.maxiOsc();
-          ${o}mod = new Module.maxiOsc();
-          ${o}.phaseReset(${p.length>3 ? p[1].loop : 0.0});
-          ${o}mod.phaseReset(${p.length>3 ? p[1].loop : 0.0});`,
-      "loop":(o,p) => `${o}.sinewave(${p[0].loop} + ${o}mod.sinewave(${p[0].loop} * ${p[1].loop}) * (${p[2].loop} * ${p[0].loop} * ${p[1].loop}))`},
-    
-  /*'oscbank': {
+  'fmsynth': {
+    "setup":(o,p) => `${o} = new Module.maxiOsc();
+      ${o}mod = new Module.maxiOsc();
+      ${o}.phaseReset(${p.length>3 ? p[1].loop : 0.0});
+      ${o}mod.phaseReset(${p.length>3 ? p[1].loop : 0.0});`,
+    "loop":(o,p) => `${o}.sinewave(${p[0].loop} + ${o}mod.sinewave(${p[0].loop} * ${p[1].loop}) * (${p[2].loop} * ${p[0].loop} * ${p[1].loop}))`},  
+  'oscbank': {
     "setup":(o,p) => 
-    `${o} = new Module.maxiOsc();
-    ${o}oscArr = [];
+    `${o} = [];
     for (let i=0; i<${p.length}; i++){
-      ${o}oscArr[i] = new Module.maxiOsc(); }`,
-    "loop":(o,p) => ``},*/
+      ${o}[i] = new Module.maxiOsc(); }`,
+    "loop":(o,p) => {let s=`(${o}[0].sinewave(${p[0].loop})`; for(let i=1; i < p.length; i++) s += `+${o}[${i}].sinewave(${p[i].loop})`; return s+")";}},
   'saw': {"setup":(o,p)=>`${o} = new Module.maxiOsc(); ${o}.phaseReset(${p.length>1 ? p[1].loop : 0.0});`, "loop":(o,p)=>`${o}.saw(${p[0].loop})`},
   'sine': {"setup":(o,p)=>`${o} = new Module.maxiOsc(); ${o}.phaseReset(${p.length>1 ? p[1].loop : 0.0});`, "loop":(o,p)=>`${o}.sinewave(${p[0].loop})`},
   'triangle': {"setup":(o,p)=>`${o} = new Module.maxiOsc(); ${o}.phaseReset(${p.length>1 ? p[1].loop : 0.0});`, "loop":(o,p)=>`${o}.triangle(${p[0].loop})`},
